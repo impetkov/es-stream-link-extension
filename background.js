6e3fa5960e6a3beca97782dce29152433d74063f
Ivan Petkov <ipetkov@appliedsystems.com>
2020-03-27 16:43:42 +0000

    Dirty working version

--- background.js
+++ background.js
@@ -1,18 +1,18 @@
-// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//         var activeTab = tabs[0];
-//         if(!activeTab){
-//             return;
-//         }
-//         chrome.tabs.sendMessage(activeTab.id, {"message": "go_to_correlation_stream"});
-//         chrome.tabs.sendMessage(activeTab.id, {"message": "go_to_policy_stream"});
-//     });
-// })
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var activeTab = tabs[0];
+        if(!activeTab){
+            return;
+        }
+        chrome.tabs.sendMessage(activeTab.id, {"message": "go_to_correlation_stream"});
+        chrome.tabs.sendMessage(activeTab.id, {"message": "go_to_policy_stream"});
+    });
+})
 
-// chrome.runtime.onMessage.addListener(
-//     function(request, sender, sendResponse) {
-//       if( request.message === "stop" ) {
-//         chrome.tabs.onUpdated.removeListener();
-//       }
-//     }
-//   );
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if( request.message === "stop" ) {
+            chrome.tabs.onUpdated.removeListener();
+        }
+    }
+);
--- content.js
+++ content.js
@@ -1,19 +1,69 @@
+async function linkCustomerCover(){
+    let customerCoverElement = document.getElementsByTagName('pre')[0];
+    while(!customerCoverElement){
+        await new Promise(r => setTimeout(r, 1000));
+        customerCoverElement = document.getElementsByTagName('pre')[0];
+    }
+    
+    if(!customerCoverElement){
+        return;
+    }
+    
+    var text = customerCoverElement.innerText.toLocaleLowerCase();
+    var customerCoverIdPositionInText = text.search('customercoverid');
+
+    if(customerCoverIdPositionInText < 0){
+        return;
+    }
+
+    var innerHTML = customerCoverElement.innerHTML.toLocaleLowerCase();
+    var customerCoverIdPositionInHTML = innerHTML.search('customercoverid');
+    if(innerHTML.substring(customerCoverIdPositionInHTML, customerCoverIdPositionInHTML+20).indexOf('<') > 0){
+        return;
+    }
+
+    var customerCoverId = text.substring(customerCoverIdPositionInText+19, customerCoverIdPositionInText+19+36);
+
+    customerCoverElement.innerHTML = customerCoverElement.innerHTML.replace(customerCoverId, '<a target="_blank" href="index.html#/streams/policy-' + customerCoverId +'">' + customerCoverId +'</a>');
+}
+
+async function linkCorrelation(){
+    let correlationElement = document.getElementsByTagName('pre')[1];
+    while(!correlationElement){
+        await new Promise(r => setTimeout(r, 1000));
+        correlationElement = document.getElementsByTagName('pre')[1];
+    }
+    
+    if(!correlationElement){
+        return;
+    }
+    
+    var text = correlationElement.innerText.toLocaleLowerCase();
+    var correlationIdPositionInText = text.search('correlationid');
+
+    if(correlationIdPositionInText < 0){
+        return;
+    }
+
+    var innerHTML = correlationElement.innerHTML.toLocaleLowerCase();
+    var correlationIdPositionInHTML = innerHTML.search('correlationid');
+    if(innerHTML.substring(correlationIdPositionInHTML, correlationIdPositionInHTML+20).indexOf('<') > 0){
+        return;
+    }
+
+    var correlationId = text.substring(correlationIdPositionInText+17, correlationIdPositionInText+17+36);
+
+    correlationElement.innerHTML = correlationElement.innerHTML.replace(correlationId, '<a target="_blank" href="index.html#/streams/correlation-' + correlationId +'">' + correlationId +'</a>');
+}
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if( request.message === "go_to_correlation_stream" ) {
-            var correlationElement = document.getElementsByTagName('pre')[1];
-            var text = correlationElement.innerText;
-            var correlationId = text.substring(text.search('correlationId')+17, text.search('correlationId')+17+36);
-
-            correlationElement.innerHTML = correlationElement.innerHTML.replace(correlationId, '<a target="_blank" href="index.html#/streams/correlation-' + correlationId +'">' + correlationId +'</a>');
+            linkCorrelation();
             chrome.runtime.sendMessage({"message": "stop"});
         }
-        if( request.message === "go_to_policy_stream" ) {
-            var customerCoverElement = document.getElementsByTagName('pre')[0];
-            var text = customerCoverElement.innerText;
-            var customerCoverId = text.substring(text.search('customerCoverId')+19, text.search('customerCoverId')+19+36);
-
-            customerCoverElement.innerHTML = customerCoverElement.innerHTML.replace(customerCoverId, '<a target="_blank" href="index.html#/streams/policy-' + customerCoverId +'">' + customerCoverId +'</a>');
+        if( request.message === "go_to_policy_stream") {
+            linkCustomerCover();
             chrome.runtime.sendMessage({"message": "stop"});
         }
     }
